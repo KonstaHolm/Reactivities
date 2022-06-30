ce6ec8435e9b65327287eafa4b3e480130ffb311
KonstaHolm <holmkonsta@gmail.com>
2022-06-30 15:53:44 +0300

    End of section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -17,7 +18,8 @@ namespace API.Controllers
          return HandleResult(await Mediator.Send(new List.Query()));
       }
 
-      [HttpGet("{id}")] // activities/id
+      [Authorize]
+      [HttpGet("{id}")]
       public async Task<IActionResult> GetActivity(Guid id)
       {
          return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
